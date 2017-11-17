518d164c922e1dfa16ea48d1c91369b8cdaecf88
v-mneszt <v-mneszt@expedia.com>
2017-11-17 12:40:47 +0100

    add gitignore and first fun js file: equals

---
+++ funfun/EqualsOrNot.js
@@ -0,0 +1,8 @@
+console.log("asdasd is not strongly equals to asdasd");
+console.log("asd‏asd" === "asdasd");
+
+console.log("asdasd is still not weakly equals to asdasd");
+console.log("asd‏asd" == "asdasd");
+
+console.log("asdasd is now equals to asdasd");
+console.log("asdasd" == "asdasd");
