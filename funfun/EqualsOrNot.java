69064bb111decc5f1c2e87676d79859fd51a28a5
v-mneszt <v-mneszt@expedia.com>
2017-11-17 12:52:39 +0100

    same in Java

---
+++ funfun/EqualsOrNot.java
@@ -0,0 +1,12 @@
+public class EqualsOrNot {
+    public static void main(String[] args) {
+        System.out.println("asdasd is not equals to asdasd");
+        System.out.println("asd‏asd".equals("asdasd"));
+
+        System.out.println("asdasd is still not equals to asdasd");
+        System.out.println("asd‏asd" == "asdasd");
+
+        System.out.println("asdasd is now equals to asdasd");
+        System.out.println("asdasd" == "asdasd");
+    }
+}
